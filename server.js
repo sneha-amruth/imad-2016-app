0b4132065e6c1c099be14df5cc9e5efb4865bf19
sneha-amruth <sneha.amruth24@gmail.com>
2016-11-25 01:54:03 -1200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send('article one requested and will be severed here');
+app.get('/poetry', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'poetry.html'));
 });
 
-app.get('/article-two', function (req, res) {
-  res.send('article two requested and will be severed here');
+app.get('/articles', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'articles.html'));
 });
 
-app.get('/article-three', function (req, res) {
-  res.send('article three requested and will be severed here');
+app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'about.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
