1df5b91c2133ed6046052f6e7e0c946925a49b5d
sneha-amruth <sneha.amruth24@gmail.com>
2016-11-26 19:37:26 -1200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(morgan('combined'));
 var pool=new Pool(config);
 app.get('/test-db', function (req, res) {
  //make a select request and return with a response
- pool.query('SELECT* FROM test', function(err,result)){
+ pool.query('SELECT* FROM test', function(err,result){
     if(err){
         res.status(500).send(err.toString());
     } 
